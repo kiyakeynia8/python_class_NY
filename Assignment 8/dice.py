88c6c087159153a9ba30e96a13a8047af674e8bb
Kiya keynia <118113533+kiyakeynia8@users.noreply.github.com>
2023-07-13 10:08:14 +0330

    Add files via upload

---
+++ Assignment 8/dice.py
@@ -0,0 +1,17 @@
+import random
+import pyfiglet
+
+title = pyfiglet.figlet_format("&--Dice--&", font="slant")
+print(title)
+
+dise_number = 0
+sum_numbers = 0
+c = 0
+
+while dise_number != 6:
+    dise_number = random.randint(1,6)
+    c += 1
+    print(f"number {c} = {dise_number}")
+    sum_numbers += dise_number
+
+print("sum = ",sum_numbers)
