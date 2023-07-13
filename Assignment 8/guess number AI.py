ded43936d95d30f80ac0d8d13c616a09d2ae7a48
Kiya keynia <118113533+kiyakeynia8@users.noreply.github.com>
2023-07-13 10:35:39 +0330

    Add files via upload

---
+++ Assignment 8/guess number AI.py
@@ -0,0 +1,24 @@
+import random
+
+while True:
+    a = int(input("To exit the game input -1 otherwise input -2 -> "))
+    number_y = int(input("enter your number : "))
+    bigـnumber = 100
+    small_number = 0
+    if a == -1:
+        break
+    while True:
+        number = random.randint(small_number, bigـnumber)
+        print("computer number = ",number)
+
+        if number_y == number:
+            print("computer win")
+            break
+
+        s = int(input("up = 1 \n down = 2 \n"))
+        
+        if s == 1:
+            small_number = number
+        
+        elif s == 2:
+            bigـnumber = number
