704ef70fefff81e1a830c22e62b16672dcb69498
Kiya keynia <118113533+kiyakeynia8@users.noreply.github.com>
2023-07-22 22:48:20 +0330

    Add files via upload

--- Assignment 11/Quadratic Equation.py
+++ Assignment 11/Quadratic Equation.py
@@ -1,9 +1,23 @@
-def Quadratic_Equation(a, x , b, c):
-    return ((a*x)**2) + (b*x) + c
+import math
 
-a = float(input("a: "))
-b = float(input("b: "))
-c = float(input("c: "))
-x = float(input("x: "))
+def Equation(a,b,c):
+    num = ((b ** 2) - ((4 * a) * c))
+    if num > 0:
+        l = []
+        s = "+"
+        m = (math.sqrt(num))
+        for i in range(2):
+            str = f"(({b} * -1) {s} {m}) / (2 * {a})"
+            str = eval(str)
+            l.append(str)
+            s = "-"
+        return l
+    elif num == 0:
+        result_1 = ((b * -1) / (2 * a))
+        return result_1
 
-print(Quadratic_Equation(a, x, b, c))
+a = float(input("enter number 1:"))
+b = float(input("enter number 2:"))
+c = float(input("enter number 3:"))
+
+print(Equation(a,b,c))
