9c0e8a939c7b7706968cd66f19883c1eed9f31b5
Kiya keynia <118113533+kiyakeynia8@users.noreply.github.com>
2023-08-15 16:38:56 +0330

    Add files via upload

---
+++ Assignment 20/class code/game.py
@@ -0,0 +1,27 @@
+import logic
+
+game_board = logic.start_game()
+
+while True:
+    inp = input("press the your command: ")
+
+    if inp == "w":
+        # move up
+        # current state
+        # update
+        # check game over
+        pass
+
+    elif inp == "s":
+        pass
+
+    elif inp == "a":
+        pass
+
+    elif inp == "d":
+        pass
+
+    else:
+        print("Invalid command!")
+
+    print(game_board)
