03c979ebb83ac28852d44d8be2eae3608da89a4d
Kiya keynia <118113533+kiyakeynia8@users.noreply.github.com>
2023-08-15 16:52:50 +0330

    Add files via upload

--- Assignment 20/class code/logic.py
+++ Assignment 20/class code/logic.py
@@ -33,7 +33,17 @@ def get_current_state(mat):
             if mat[i][j] == 0:
                 return "Game Not Ever"
 
-    for i in range(4):
-        for j in range(4):
+    for i in range(3):
+        for j in range(3):
             if (mat[i][j] == mat[i+1][j]) or (mat[i][j] == mat[i][j+1]):
                 return "Game Not Ever"
+
+    for i in range(3):
+        if mat[3][i] == mat[3][i+1]:
+            return "Game Not Ever"
+
+    for i in range(3):
+        if mat[i][3] == mat[i+1][3]:
+            return "Game Not Ever"
+
+    return "lose"
